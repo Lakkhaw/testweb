8cdb40df6da66e1b53521da32c210f98ab00c544
Lakkhaw <113591735+Lakkhaw@users.noreply.github.com>
2022-11-30 13:00:24 +0700

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,202 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="en-GB" dir="ltr">
+	<head>
+		<title>Search - New Project 13</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.1.1 - www.websitex5.com" />
+		<meta property="og:locale" content="en" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://wdddd.com/imsearch.php" />
+		<meta property="og:title" content="Search" />
+		<meta property="og:site_name" content="New Project 13" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-1-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-1-1" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-1-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-1-1" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?14-0-1-1-638053296363103038" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-1-1"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-1-1" data-files-version="14-0-1-1"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Your browser does not support the features necessary to display this website.','Your browser may not support the features necessary to display this website.','[1]Update your browser[/1] or [2]continue without updating[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">Search - New Project 13</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_1" class="" > <div id="imHeader_imCellStyleGraphics_1"></div><div id="imHeader_imCellStyle_1" ><div id="imHeader_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+หน้าหลัก		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3-1.html">
+ที่ท่องเที่ยว		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4-1.html">
+ประวัติ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-5.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-5.html">
+ตราจังหวัด		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-6-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6-1.html">
+คำขวัญ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-7.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-7.html">
+อั้ลบั้มรูปภาพ		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-8.php">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-8.php">
+ร้องเรียน		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_01',1000)});
+$(function () {$('#imHeader_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0, subtimeout = 0, width = 'none', height = 'none';        var submenu = $this.children('ul').add($this.find('.multiple-column > ul'));    $this.on('mouseenter', function () {        clearTimeout(timeout);        clearTimeout(subtimeout);        $this.children('.multiple-column').show(0);        submenu.stop(false, false);        if (width == 'none') {             width = submenu.width();        }        if (height == 'none') {            height = submenu.height();            submenu.css({ overflow : 'hidden', height: 0});        }        setTimeout(function () {         submenu.css({ overflow : 'hidden'}).fadeIn(1).animate({ height: height }, 300, null, function() {$(this).css('overflow', 'visible'); });        }, 250);    }).on('mouseleave', function () {        timeout = setTimeout(function () {         submenu.stop(false, false);            submenu.css('overflow', 'hidden').animate({ height: 0 }, 300, null, function() {$(this).fadeOut(0); });            subtimeout = setTimeout(function () { $this.children('.multiple-column').hide(0); }, 300);        }, 250);    });});});
+
+</script>
+</div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_03_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_3" class="" > <div id="imStickyBar_imCellStyleGraphics_3"></div><div id="imStickyBar_imCellStyle_3" ><div id="imStickyBar_imMenuObject_03"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+หน้าหลัก		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3-1.html">
+ที่ท่องเที่ยว		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4-1.html">
+ประวัติ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-5.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-5.html">
+ตราจังหวัด		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-6-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6-1.html">
+คำขวัญ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-7.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-7.html">
+อั้ลบั้มรูปภาพ		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-8.php">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-8.php">
+ร้องเรียน		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_03',1000)});
+$(function () {$('#imStickyBar_imMenuObject_03 ul li').each(function () {    var $this = $(this), timeout = 0, subtimeout = 0, width = 'none', height = 'none';        var submenu = $this.children('ul').add($this.find('.multiple-column > ul'));    $this.on('mouseenter', function () {        clearTimeout(timeout);        clearTimeout(subtimeout);        $this.children('.multiple-column').show(0);        submenu.stop(false, false);        if (width == 'none') {             width = submenu.width();        }        if (height == 'none') {            height = submenu.height();            submenu.css({ overflow : 'hidden', height: 0});        }        setTimeout(function () {         submenu.css({ overflow : 'hidden'}).fadeIn(1).animate({ height: height }, 300, null, function() {$(this).css('overflow', 'visible'); });        }, 250);    }).on('mouseleave', function () {        timeout = setTimeout(function () {         submenu.stop(false, false);            submenu.css('overflow', 'hidden').animate({ height: 0 }, 300, null, function() {$(this).fadeOut(0); });            subtimeout = setTimeout(function () { $this.children('.multiple-column').hide(0); }, 300);        }, 250);    });});});
+
+</script>
+</div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Skip the main menu">Go to content</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"><div id="imSideBar_imObjectImage_01_wrapper" class="template-object-wrapper"><div id="imSideBar_imCell_1" class="" > <div id="imSideBar_imCellStyleGraphics_1"></div><div id="imSideBar_imCellStyle_1" ><img id="imSideBar_imObjectImage_01" src="images/empty-GT_imagea-1-.png" title="" alt="" /></div></div></div></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Search results</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Read this page again">Back to content</a> | <a href="#imGoToMenu" title="Remain on this page">Back to main menu</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">To use this website you must enable JavaScript.</div></noscript>
+	</body>
+</html>
---
+++ page-8.php
@@ -0,0 +1,282 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="en-GB" dir="ltr">
+	<head>
+		<title>ร้องเรียน - New Project 13</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.1.1 - www.websitex5.com" />
+		<meta property="og:locale" content="en" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://wdddd.com/page-8.php" />
+		<meta property="og:title" content="ร้องเรียน" />
+		<meta property="og:site_name" content="New Project 13" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-1-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-1-1" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-1-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-1-1" media="screen" />
+		<link rel="stylesheet" type="text/css" href="appsresources/css/style.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="pluginAppObj_8_01/css/custom.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="pluginAppObj_8_04/css/custom.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="pcss/page-8.css?14-0-1-1-638053296386845743" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-1-1"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-1-1" data-files-version="14-0-1-1"></script>
+		<script type="text/javascript" src="appsresources/js/main.js"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Your browser does not support the features necessary to display this website.','Your browser may not support the features necessary to display this website.','[1]Update your browser[/1] or [2]continue without updating[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'page-8.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">ร้องเรียน - New Project 13</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_1" class="" > <div id="imHeader_imCellStyleGraphics_1"></div><div id="imHeader_imCellStyle_1" ><div id="imHeader_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+หน้าหลัก		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3-1.html">
+ที่ท่องเที่ยว		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4-1.html">
+ประวัติ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-5.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-5.html">
+ตราจังหวัด		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-6-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6-1.html">
+คำขวัญ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-7.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-7.html">
+อั้ลบั้มรูปภาพ		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-8.php">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-8.php">
+ร้องเรียน		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_01',1000)});
+$(function () {$('#imHeader_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0, subtimeout = 0, width = 'none', height = 'none';        var submenu = $this.children('ul').add($this.find('.multiple-column > ul'));    $this.on('mouseenter', function () {        clearTimeout(timeout);        clearTimeout(subtimeout);        $this.children('.multiple-column').show(0);        submenu.stop(false, false);        if (width == 'none') {             width = submenu.width();        }        if (height == 'none') {            height = submenu.height();            submenu.css({ overflow : 'hidden', height: 0});        }        setTimeout(function () {         submenu.css({ overflow : 'hidden'}).fadeIn(1).animate({ height: height }, 300, null, function() {$(this).css('overflow', 'visible'); });        }, 250);    }).on('mouseleave', function () {        timeout = setTimeout(function () {         submenu.stop(false, false);            submenu.css('overflow', 'hidden').animate({ height: 0 }, 300, null, function() {$(this).fadeOut(0); });            subtimeout = setTimeout(function () { $this.children('.multiple-column').hide(0); }, 300);        }, 250);    });});});
+
+</script>
+</div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_03_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_3" class="" > <div id="imStickyBar_imCellStyleGraphics_3"></div><div id="imStickyBar_imCellStyle_3" ><div id="imStickyBar_imMenuObject_03"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+หน้าหลัก		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3-1.html">
+ที่ท่องเที่ยว		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4-1.html">
+ประวัติ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-5.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-5.html">
+ตราจังหวัด		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-6-1.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6-1.html">
+คำขวัญ		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-7.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-7.html">
+อั้ลบั้มรูปภาพ		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-8.php">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-8.php">
+ร้องเรียน		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_03',1000)});
+$(function () {$('#imStickyBar_imMenuObject_03 ul li').each(function () {    var $this = $(this), timeout = 0, subtimeout = 0, width = 'none', height = 'none';        var submenu = $this.children('ul').add($this.find('.multiple-column > ul'));    $this.on('mouseenter', function () {        clearTimeout(timeout);        clearTimeout(subtimeout);        $this.children('.multiple-column').show(0);        submenu.stop(false, false);        if (width == 'none') {             width = submenu.width();        }        if (height == 'none') {            height = submenu.height();            submenu.css({ overflow : 'hidden', height: 0});        }        setTimeout(function () {         submenu.css({ overflow : 'hidden'}).fadeIn(1).animate({ height: height }, 300, null, function() {$(this).css('overflow', 'visible'); });        }, 250);    }).on('mouseleave', function () {        timeout = setTimeout(function () {         submenu.stop(false, false);            submenu.css('overflow', 'hidden').animate({ height: 0 }, 300, null, function() {$(this).fadeOut(0); });            subtimeout = setTimeout(function () { $this.children('.multiple-column').hide(0); }, 300);        }, 250);    });});});
+
+</script>
+</div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Skip the main menu">Go to content</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"><div id="imSideBar_imObjectImage_01_wrapper" class="template-object-wrapper"><div id="imSideBar_imCell_1" class="" > <div id="imSideBar_imCellStyleGraphics_1"></div><div id="imSideBar_imCellStyle_1" ><img id="imSideBar_imObjectImage_01" src="images/empty-GT_imagea-1-.png" title="" alt="" /></div></div></div></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imGroup_1" class="imVGroup">
+				<div id="imPageRow_1" class="imPageRow">
+				
+				<div id="imPageRowContent_1" class="imContentDataContainer">
+				<div id="imCell_1" class="animable fadeInUp" > <div id="imCellStyleGraphics_1"></div><div id="imCellStyle_1"  data-responsive-sequence-number="1"><div id="pluginAppObj_8_01">
+				
+					  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span class="cd-words-wrapper"><b class="is-visible">สามารถร้องเรียนได้ที่นี่</b></span></p></div>
+				   </div></div></div>
+				<script type="text/javascript">
+					x5engine.boot.push(function () {
+						x5engine.utils.onElementInViewPort($('#imCell_1').get(0), function () {
+							x5engine.utils.revealObject($("#imCell_1"))
+						}, 100);
+					});
+				</script>
+				</div>
+				</div>
+				<div id="imPageRow_2" class="imPageRow">
+				
+				<div id="imPageRowContent_2" class="imContentDataContainer">
+				<div id="imCell_2" class="animable fadeInUp" > <div id="imCellStyleGraphics_2"></div><div id="imCellStyle_2"  data-responsive-sequence-number="2"><form id="imObjectForm_2" action="imemail/imEmailForm.php" method="post" enctype="multipart/form-data">
+					<fieldset class="first">
+						<div>
+							<div id="imObjectForm_2_1_container" class="imObjectFormFieldContainer"><label id="imObjectForm_2_1_label" for="imObjectForm_2_1">รับเรื่องเรียน</label><br /><div id="imObjectForm_2_1_field">
+				<input type="text" class="valEmail" id="imObjectForm_2_1" name="imObjectForm_2_1" /></div>
+				</div>
+						</div>
+					</fieldset>
+					<fieldset>
+						<input type="text" id="imObjectForm_2_prot" name="imSpProt" />
+					</fieldset>
+					<div id="imObjectForm_2_buttonswrap">
+						<input type="submit" value="Send" />
+					</div>
+				</form>
+				<script>x5engine.boot.push('x5engine.imForm.initForm(\'#imObjectForm_2\', false, {jsid: \'7EAEA17CF86182C92AE2947C4FB69692\', type: \'tip\', showAll: true, classes: \'validator\', landingPage: \'index.html\', labelColor: \'rgba(0, 0, 0, 1)\', fieldColor: \'rgba(0, 0, 0, 1)\', selectedFieldColor: \'rgba(0, 0, 0, 1)\'})');</script>
+				</div></div>
+				<script type="text/javascript">
+					x5engine.boot.push(function () {
+						x5engine.utils.onElementInViewPort($('#imCell_2').get(0), function () {
+							x5engine.utils.revealObject($("#imCell_2"))
+						}, 100);
+					});
+				</script>
+				<div id="imCell_4" class="animable fadeInUp" > <div id="imCellStyleGraphics_4"><div style="position: relative; height: 100%;"><div id="imCellStyleGraphicsC_4"></div><div id="imCellStyleGraphicsT_4"></div><div id="imCellStyleGraphicsTL_4"></div><div id="imCellStyleGraphicsTR_4"></div><div id="imCellStyleGraphicsL_4"></div><div id="imCellStyleGraphicsR_4"></div><div id="imCellStyleGraphicsB_4"></div><div id="imCellStyleGraphicsBL_4"></div><div id="imCellStyleGraphicsBR_4"></div></div></div><div id="imCellStyle_4"  data-responsive-sequence-number="3"><div id="pluginAppObj_8_04">
+				
+					  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span class="cd-words-wrapper"><b class="is-visible">โหวตความพึงพอใจได้ที่นี่</b></span></p></div>
+				   </div></div></div>
+				<script type="text/javascript">
+					x5engine.boot.push(function () {
+						x5engine.utils.onElementInViewPort($('#imCell_4').get(0), function () {
+							x5engine.utils.revealObject($("#imCell_4"))
+						}, 100);
+					});
+				</script>
+				</div>
+				</div>
+				<div id="imPageRow_3" class="imPageRow">
+				
+				<div id="imPageRowContent_3" class="imContentDataContainer">
+				<div id="imCell_3" class="animable fadeInUp" > <div id="imCellStyleGraphics_3"></div><div id="imCellStyle_3"  data-responsive-sequence-number="4"><div id="GuestBookObject_83" style="height: 400px; overflow-y: auto; overflow-x: hidden;">
+				<?php
+				$gb = new ImTopic('x5gb83');
+				$gb->setCommentsPerPage(1);
+				$gb->setTitle('ร้องเรียน');
+				$gb->loadXML(pathCombine(array($imSettings['general']['public_folder'], '')));
+				$gb->showRating();
+				?>
+				<script type="text/javascript">x5engine.boot.push('x5engine.topic({ target: \'#GuestBookObject_83\', scrollbar: true})', false, 6);</script>
+				</div>
+				</div></div>
+				<script type="text/javascript">
+					x5engine.boot.push(function () {
+						x5engine.utils.onElementInViewPort($('#imCell_3').get(0), function () {
+							x5engine.utils.revealObject($("#imCell_3"))
+						}, 100);
+					});
+				</script>
+				</div>
+				</div>
+				<div id="imPageRow_4" class="imPageRow">
+				
+				<div id="imPageRowContent_4" class="imContentDataContainer">
+				<div id="imGroup_2" class="imEGroup"></div>
+				</div>
+				</div>
+				
+				</div>
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Read this page again">Back to content</a> | <a href="#imGoToMenu" title="Remain on this page">Back to main menu</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">To use this website you must enable JavaScript.</div></noscript>
+	</body>
+</html>
